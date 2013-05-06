271fc797d7339f9bf41cd28879ff21088fb974e9
David Rouleau <Shabbypenguin@gmail.com>
2013-05-06 10:39:46 -0400

    Add Auto-root: Thanks to koush https://github.com/CyanogenMod/android_bootable_recovery/commit/9edb35905d5d6a3f21f107e9eff630b36e16d9ec

--- Android.mk
+++ Android.mk
@@ -181,4 +181,5 @@ include $(commands_recovery_local_path)/edify/Android.mk
 include $(commands_recovery_local_path)/updater/Android.mk
 include $(commands_recovery_local_path)/applypatch/Android.mk
 include $(commands_recovery_local_path)/utilities/Android.mk
+include $(commands_recovery_local_path)/su/Android.mk
 commands_recovery_local_path :=
